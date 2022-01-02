a66027fec61102eb4d0b6c0c7a537aa284c60466
Fauzi Galih Aji Saputro <fauzigalihas@gmail.com>
2022-01-02 09:32:31 +0700

    Number

---
+++ src/Number.kt
@@ -0,0 +1,23 @@
+fun main() {
+    var txtByte: Byte = 100
+    var txtShort: Short = 10000
+    var txtInt: Int = 5000000
+    var txtLong: Long = 5000000000L
+
+    // Decimal
+    var txtFloat: Float = 10.5F
+    var txtDouble: Double = 10.55
+
+    // Literal
+    var decimalLiteral: Int = 100
+    var hexaDecimal: Int = 0xFF
+    var binaryLiteral: Int = 0b011000011
+
+    // Underscore (Separator / Tanda Baca)
+    var rupiah: Int = 100_000_000
+    var rupiahLong: Long = 10_000_000_000L
+
+    // Conversion
+    var intToDouble: Double = txtInt.toDouble()
+    var floatToInt: Int = txtFloat.toInt()
+}
