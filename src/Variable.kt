5bc317f7c5fc528fbef00aae0a18b572bb0d0515
Fauzi Galih Aji Saputro <fauzigalihas@gmail.com>
2022-01-04 08:48:04 +0700

    Variable

---
+++ src/Variable.kt
@@ -0,0 +1,11 @@
+fun main() {
+    // Variable constant (not editable)
+    val valVariable: Int = 10
+
+    // Variable can editable
+    var varVariable: Int = 20
+    varVariable += 10
+
+    println(valVariable)
+    println(varVariable)
+}
