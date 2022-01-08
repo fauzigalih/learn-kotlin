870c141f24c97a4be880e0e8fd0ec1b5056636b4
Fauzi Galih Aji Saputro <fauzigalihas@gmail.com>
2022-01-08 22:45:21 +0700

    Variable

--- src/Variable.kt
+++ src/Variable.kt
@@ -1,11 +1,22 @@
+// Constanta
+const val TITLE: String = "Variable"
+const val VERSION: Float = 1.0F
+
 fun main() {
     // Variable constant (not editable)
     val valVariable: Int = 10
+    println(valVariable)
 
     // Variable can editable
     var varVariable: Int = 20
-    varVariable += 10
-
-    println(valVariable)
+    varVariable += valVariable
     println(varVariable)
+
+    // Nullable
+    var variableNull: String? = null
+    variableNull = "Fauzi"
+    println(variableNull?.length)
+
+    // Constanta Print
+    println("This in file $TITLE with version $VERSION")
 }
